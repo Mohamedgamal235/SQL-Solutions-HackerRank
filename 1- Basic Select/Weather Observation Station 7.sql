95447b2c7cbb4655f1c39ae170dff52ac780df98
Mohamed Gamal Ali <gemmymo6@gmail.com>
2025-02-27 23:59:16 +0200

    Update Code

--- 1- Basic Select/Weather Observation Station 7.sql
+++ 1- Basic Select/Weather Observation Station 7.sql
@@ -1,3 +1,3 @@
 SELECT DISTINCT(CITY) 
 FROM STATION 
-WHERE CITY LIKE '%a' or  CITY LIKE '%e' or CITY LIKE '%i' or CITY LIKE '%o' or CITY LIKE '%u' ;
+WHERE CITY LIKE '%a' or  CITY LIKE '%e' or CITY LIKE '%i' or CITY LIKE '%o' or CITY LIKE '%u'  ;
